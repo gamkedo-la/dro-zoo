5e45fd3c8b9b84051939b8498dad28630c3d84f3
Antti Lipsanen <a.lipsanen@gmail.com>
2021-01-18 21:02:15 +0200

    MuteAudio script created, mute key currently bound to L

---
+++ Assets/Scripts/MuteAudio.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MuteAudio : MonoBehaviour
+{
+
+    private bool isMuted = false;
+    private float waitForSFX = 0f;
+    private float originalVolume;
+    public AudioClip muteButtonAudio;
+
+    private void OnPlayerAudio(){
+        HandleMute();
+    }
+
+    public void HandleMute()
+    {
+        //mutes all audio by lowering Audio Listener's volume to 0
+        PlayButtonAudio();
+        if(!muteButtonAudio) {
+            waitForSFX = 0f;
+        } else {
+            waitForSFX = muteButtonAudio.length;
+        }
+
+        Invoke("ToggleMute", waitForSFX);
+                
+    }
+
+    private void PlayButtonAudio(){
+        if(!muteButtonAudio) {
+            return;
+        }
+
+        AudioSource.PlayClipAtPoint(muteButtonAudio, Camera.main.transform.position);
+    }
+
+    private void ToggleMute()
+    {
+        isMuted = !isMuted;
+        if (isMuted) {
+            originalVolume = AudioListener.volume; //if we are muting, store original value
+            AudioListener.volume = 0;
+        } else {
+            AudioListener.volume = originalVolume;
+            PlayButtonAudio();
+        }
+    }
+
+}
