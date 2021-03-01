f5312bc91d52c25d4d97ba6890d988ab17ee604d
deepbluezen <deepbluezen@users.noreply.github.com>
2021-03-01 19:38:42 +0000

    robot arm gradually moves to grab panel control as moves close enough

--- Assets/ikboneweight.cs
+++ Assets/ikboneweight.cs
@@ -11,11 +11,22 @@ public class ikboneweight : MonoBehaviour
     private TwoBoneIKConstraint constraint;
      [SerializeField]
     private float weight;
+
+    [SerializeField]
+     private Transform target;
+     [SerializeField]
+     private Transform source;
+
+
     // Needs to be in Update not LateUpdate due to order animation is assessed
     void Update()
     {
-        
-        constraint.data.targetPositionWeight = weight;
-
+        float dist = Vector3.Distance(target.position, transform.position);
+        print("Distance to target: " + dist);
+        if (dist > 4.2f) {
+            constraint.data.targetPositionWeight = 0f;
+        }  else{
+        constraint.data.targetPositionWeight = weight * (1 -(dist - 3.2f));
+        }
     }
 }
