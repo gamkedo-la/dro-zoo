0b41c69c6b64368b0594afe84a1753a923da0f44
deepbluezen <deepbluezen@users.noreply.github.com>
2021-03-02 13:08:16 +0000

    dolly camera set up

--- Assets/ikboneweight.cs
+++ Assets/ikboneweight.cs
@@ -19,14 +19,15 @@ public class ikboneweight : MonoBehaviour
 
 
     // Needs to be in Update not LateUpdate due to order animation is assessed
+    
     void Update()
     {
-        float dist = Vector3.Distance(target.position, transform.position);
+        float dist = Vector3.Distance(target.position, source.position);
         print("Distance to target: " + dist);
-        if (dist > 4.2f) {
+        if (dist > 1.4) {
             constraint.data.targetPositionWeight = 0f;
         }  else{
-        constraint.data.targetPositionWeight = weight * (1 -(dist - 3.2f));
+        constraint.data.targetPositionWeight = weight * (1 -(dist - 0.8f));
         }
     }
 }
